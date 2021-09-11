214c8d8b156d52d8748d0b5bacdc61288cb4f99a
Dennis <dennis.yakovlev@yahoo.ca>
2021-09-10 21:07:47 -0400

    basic jsdoc remover

--- removeComment.py
+++ removeComment.py
@@ -1,12 +1,54 @@
 import os
+import re
+
+# Add options to remove jsdoc, comments, and line spacing seperately
+
+def _find_startJsDoc(s):
+    ''' Find if <s> has starting jsdoc pattern
+    '''
+
+    return re.search('^\s*\/\*{2}', s)
+
+def _find_endJsDoc(s):
+    ''' Find if <s> has ending jsdoc pattern
+    '''
+
+    return re.search('\*\/$', s)
 
 def _convertFile(dir):
     ''' Remove comments from file.
     '''
-    f = open(dir)
+    f = open(dir, 'r')
 
-    for line in f:
-        print(line)
+    arrFile = []
+
+    line = ' '
+    while line:
+        line = f.readline()
+
+        if (_find_startJsDoc(line)):
+            while line and not _find_endJsDoc(line):
+                line = f.readline()
+            line = f.readline()
+
+        arrFile.append(line)
+
+    f.close()
+
+    return arrFile
+
+def _wrtieFile(dir, name, arr):
+
+    f = open(os.path.join(dir, name + '.js'), 'w')
+
+    f.writelines(arr)
+
+    f.close()
+
+def _createDir(dir):
+
+    if not os.path.exists(dir):
+        os.mkdir(dir)
 
 # depth first search for files
 def start(obj, start_path = '', path = ''):
@@ -14,7 +56,7 @@ def start(obj, start_path = '', path = ''):
 
         <obj> object of format 
         {
-            directory: [fileName, ...]
+            directory: [fileName, ...],
             directory2: [
                 {
                     directory3: [fileName, ...],
@@ -24,21 +66,35 @@ def start(obj, start_path = '', path = ''):
             ]
         }
 
-              where fileName is the name of file to be converted. .js is not needed
+              where fileName is dict
+              {
+                  name: fileName,
+                  jsdoc: bool, 
+                  single: bool,
+                  multi: bool
+              }
+              name - the name of file to be converted. .js extension should NOT be added
+              jsdoc - True to remove jsdoc comment
+              single - True to remove single line // comments
+              multi - True to remove multi line /* */ comments
         <start_path> the base directory to begin searching for files from
     '''
+    
+    outPath = os.path.join(start_path, 'out', path) # path in the output folder
+
+    if path == '':
+        _createDir(outPath)
 
     if isinstance(obj, str):
-        _convertFile(os.path.join(start_path, path, obj + '.js'))
+        arr = _convertFile(os.path.join(start_path, path, obj + '.js'))
+        _wrtieFile(outPath, obj, arr)
         return
 
+
     for key in obj:
+        _createDir(os.path.join(outPath, key))
+
         elem = obj[key]
 
         for item in elem:
             start(item, start_path, os.path.join(path, key))
-
-
-
-
-
